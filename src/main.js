d0c0be68f7c9f2392ac67c3854d3a08581943315
mister_mo <825305706@qq.com>
2022-02-19 22:34:49 +0800

    element-ui和echarts引入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import './plugins/element'
+import ECharts from 'echarts'
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = ECharts
 
 new Vue({
   router,
---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Select } from 'element-ui'
+
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+/* 或写为
+ * Vue.use(Button)
+ * Vue.use(Select)
+ */
