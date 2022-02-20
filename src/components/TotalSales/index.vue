532b908452a599ac9acac44d1cde40898b28930b
mister_mo <825305706@qq.com>
2022-02-20 16:10:56 +0800

    累计销售额组件开发

--- src/components/TotalSales/index.vue
+++ src/components/TotalSales/index.vue
@@ -1,13 +1,22 @@
 <template>
   <common-card title="累计销售额" value="￥32,039,165">
     <template>
-      <div class="compare"></div>
+      <div class="compare-wrapper">
+        <div class="compare">
+          <span>日同比</span>
+          <span class="emphasis">7.33%</span>
+          <div class="increase"></div>
+        </div>
+        <div class="compare">
+          <span>月同比</span>
+          <span class="emphasis">7.33%</span>
+          <div class="decrease"></div>
+        </div>
+      </div>
     </template>
     <template v-slot:footer>
-      <div>
-        <span>昨日销售额</span>
-        <span class="money">￥30,000,000</span>
-      </div>
+      <span>昨日销售额</span>
+      <span class="emphasis">￥30,000,000</span>
     </template>
   </common-card>
 </template>
@@ -23,14 +32,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .compare {
+  .compare-wrapper {
     height: 100%;
-    background: yellow;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .compare {
+      display: flex;
+      align-items: center;
+      font-size: 12px;
+      margin-top: 3px;
+      color: #666;
+      .increase {
+        width: 0;
+        height: 0;
+        border-width: 3px;
+        border-color: transparent transparent red transparent;
+        border-style: solid;
+        margin: 0 0 3px 5px;
+      }
+      .decrease {
+        width: 0;
+        height: 0;
+        border-width: 3px;
+        border-color: green transparent transparent transparent;
+        border-style: solid;
+        margin: 3px 0 0 5px;
+      }
+    }
   }
-  span {
-    font-size: 12px;
-  }
-  .money {
+  .emphasis {
     margin-left: 5px;
     color: #333;
     font-weight: 700;
